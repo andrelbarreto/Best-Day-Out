cf46cc313f446f7c98b690eb493834b66a8602c9
gideonrynn <sarah.c.dillard@gmail.com>
2019-12-07 08:02:02 -0600

    updated comments

--- weather-api.js
+++ weather-api.js
@@ -1,8 +1,7 @@
 //version 1: Weather API
-
-//Goal: when search button is clicked, run function that will use information entered by user to query Weatherbit.io
-//steps listed in pseudocode-weatherapi.txt
-
+//Goal: when search button is clicked, run functions that will:
+//1: check user input date against current date and select current weather query or forecast query
+//2: use information entered by user to query Weatherbit.io
 
 //define variable for html search button
 var searchButton = $(".button");
@@ -20,18 +19,18 @@ var APIKey = "faa9f8bb779e4165b52c0af7edcdbf68";
 
 //define current date using a moment
 var currentDate = moment().format('YYYY-MM-DD');
-console.log('This is the current date: ' + currentDate);
+console.log('WA: This is the current date: ' + currentDate);
 
 
 //**CLICK EVENT**//
 
-//create on click event trigged by submit button that will check input date and initiate queries
+//create on click event trigged by main age Search button that will check user inputs and initiate queries
 searchButton.on("click", selectQuery);
 
 
-//**FUNCTIONS/AJAX CALLS**//
+//**FUNCTIONS & AJAX CALLS**//
 
-//function will check user input date against current date and select current weather query or forecast query
+//check user input date against current date and select current weather query or forecast query
 function selectQuery () {
    
     //define variable that captures the city, state and date entered by the user
@@ -40,9 +39,9 @@ function selectQuery () {
     dateInput = $('#date').val();
 
     //console log result of user entry
-    console.log('This is the state entered: ' + dateInput);
-    console.log('This is the city entered: ' + cityInput);
-    console.log('This is the date entered: ' + stateInput);
+    console.log('WA: This is the state entered: ' + dateInput);
+    console.log('WA: This is the city entered: ' + cityInput);
+    console.log('WA: This is the date entered: ' + stateInput);
 
     if (dateInput === currentDate) {
         console.log('these dates are the same');
@@ -58,6 +57,7 @@ function selectQuery () {
 // end select query function
 }
 
+//query current weather conditions and render details
 function queryCurrentWeatherAPI () {
 
     //define variable that represents current conditions query info
@@ -85,9 +85,10 @@ function queryCurrentWeatherAPI () {
 //end function
 }
 
+//query forecast weather conditions and render details
 function queryForecastAPI () {
 
-    //define variable that represents current conditions query info
+    //define variable that represents forecast conditions query info
     var queryForecast = "https://api.weatherbit.io/v2.0/forecast/daily?" + "city=" + cityInput + "," + stateInput + "&units=I" + "&key=" + APIKey;
 
     $.ajax({
