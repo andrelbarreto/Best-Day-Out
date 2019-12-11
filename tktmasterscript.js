3a0a8cc18175dd17873d6a17efce61ef2a94a4ae
Andrelbarreto <andrebarreto77@gmail.com>
2019-12-10 20:27:54 -0600

    Updated tktmaster widget

--- tktmasterscript.js
+++ tktmasterscript.js
@@ -1,4 +1,6 @@
 var page = 0;
+//gets a copy of original HTML because Ticketmaster widget overwrites it
+var ticketMatsterWidgetTemplate = $(document.getElementById('Ticketmaster-widget').outerHTML);
 // define variables for search button that captures the city, state, date and category indicated by user, by default are chosen to show first card
 var searchButton = $(".button");
 var cityI= "Chicago";
@@ -11,6 +13,7 @@ var TktAPIKey = "MRyvwbGL4H4yvINfi4pGByvFdAPc4yrC";
 searchButton.on("click", function() {
   selectQuery();
   getEvents(page);
+  reloadTicketmasterWidget();
 });
 
 
@@ -133,5 +136,28 @@ function showAttraction(json) {
   $('#attraction img').first().attr('src',json.images[0].url);
   $('#classification').text(json.classifications[0].segment.name + " - " + json.classifications[0].genre.name + " - " + json.classifications[0].subGenre.name);
 }
+
+
+// deletes the old widget, appends a new one based on the template variable, and reruns the ticketmaster script
+function reloadTicketmasterWidget() {
+    // reset the widget
+    $('#Ticketmaster-widget').fadeOut(400, function() {
+        // creates a new template, sets id and city
+        var newTemplate = ticketMatsterWidgetTemplate;
+        newTemplate.attr('w-city',cityI);
+        // writes new html over the existing widget
+        $('#Ticketmaster-widget').html(newTemplate);
+        // this will run the script from ticketmaster that converts our html
+        // into the ticketmaster widget
+        var s = document.createElement('script');
+        s.src = 'https://ticketmaster-api-staging.github.io/products-and-docs/widgets/event-discovery/1.0.0/lib/main-widget.js';
+        document.body.appendChild(s);
+        $('#Ticketmaster-widget').fadeIn(400);
+    })
+}
+// put this in after the new search, modify it
+//setTimeout(() => {
+ //   reloadTicketmasterWidget();
+//}, 2000);
 
 getEvents(page);
